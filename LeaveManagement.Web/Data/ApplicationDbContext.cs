142a09161779f1c1d96f3a06ed1dcf686786ef84
pc <pc@DESKTOP-CPNNPBS>
2025-02-27 16:30:06 -0400

    Add project files.

---
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+}
